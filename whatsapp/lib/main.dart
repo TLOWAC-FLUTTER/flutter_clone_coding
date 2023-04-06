2e1d12c89c38278116459b5f9879187731f60555
ChangHun Lee <leechanghun55@gmail.com>
2023-04-06 11:07:48 +0900

    refactor(page) : welcome 페이지 분리

--- whatsapp/lib/main.dart
+++ whatsapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/featuure/welcome/pages/welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Whats App',
       home: Scaffold(
         body: Center(
-          child: Text('Demo '),
+          child: WelcomePage(),
         ),
       ),
     );
